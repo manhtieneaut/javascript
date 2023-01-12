7ca27d1316c1e63fa25937f79b0c733216cf74ea
manhtieneaut <92147195+manhtieneaut@users.noreply.github.com>
2023-01-12 12:15:31 +0700

    Update script.js

--- homeword1/script.js
+++ homeword1/script.js
@@ -1,10 +1,26 @@
-function calculater(){
+function calculater() {
     let number1 = prompt("HEX you want:");
 
     const hexToDecimal = hex => parseInt(hex, 16);
     const dec1 = hexToDecimal(number1);
-    alert(dec1); // 2
-    
 
-  
+    const hexToBinary = decimal => {
+        let result = "";
+        if (decimal == 0) {
+            result = "0";
+        }
+        while (decimal > 0) {
+            result += decimal % 2;
+            decimal = Math.floor(decimal / 2);
+        };
+        return result.split("").reverse().join("");
+    }
+
+    const dec2 = hexToBinary(dec1);
+
+    alert("Dạng thập phân: " + dec1); // 10
+    alert("Dạng nhị phân: " + dec2); // 2
+
+
+
 }
